d0208b33ed6bd2acd9001254242c78f538d41040
egkoppel <egluschovekoppel1@gmail.com>
2016-11-01 20:18:16 +0000

    Initial Commit

---
+++ test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  test
+//
+//  Created by Eliyahu Gluschove-Koppel on 01/11/2016.
+//  Copyright © 2016 Eliyahu Gluschove-Koppel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Eliyahu Gluschove-Koppel on 01/11/2016.
+//  Copyright © 2016 Eliyahu Gluschove-Koppel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
